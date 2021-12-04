1fe46455a29855ad7ac2c757715dacc53ce241ab
chenleiv <chenleiv@protonmail.com>
2021-12-04 10:52:08 +0200

    side-menu

--- src/cmps/aside-menu.vue
+++ src/cmps/aside-menu.vue
@@ -1,12 +1,19 @@
 <template>
-    <section class="aside-menu">
-        <h4>menu</h4>
-        <!-- <button>delete</button> -->
-    </section>
+    <div>
+        <div>
+            <main class="aside-menu">
+                <button>X</button>
+                <p>menu</p>
+                <p>Change backgroundColor</p>
+                <p>Archive</p>
+                <p>Activity</p>
+            </main>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'asideMenu',
-    };
+    export default {};
 </script>
+
+<style></style>
