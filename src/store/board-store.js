2520d941fb9fb268e89b2571945aadff14464df8
Or Baadani <orbaadani@gmail.com>
2022-01-18 19:36:53 +0200

    conflict

--- src/store/board-store.js
+++ src/store/board-store.js
@@ -272,7 +272,6 @@ export const boardStore = {
 
         async updateTask({ commit }, { boardId, groupId, task }) {
             try {
-<<<<<<< HEAD
                 const newBoard = await boardService.updateTask(
                     boardId,
                     groupId,
@@ -283,17 +282,7 @@ export const boardStore = {
                 socketService.off('update-board');
                 socketService.on(SOCKET_EVENT_BOARD_UPDATED, (newBoard) => {
                     commit({ type: 'getBoard', board: newBoard });
-                    console.log('%c updateBoard', 'background: yellow');
                 });
-=======
-                const newBoard = await boardService.updateTask(boardId, groupId, task);
-                commit({ type: 'updateBoard', board: newBoard })
-                socketService.emit('update-board', newBoard)
-                socketService.off('update-board')
-                socketService.on(SOCKET_EVENT_BOARD_UPDATED, newBoard => {
-                    commit({ type: 'getBoard', board: newBoard })
-                })
->>>>>>> 23a9a931bc59f5df3d8e6c4740b5a3687f06c9d2
             } catch (err) {
                 console.log('updateTask Error (Store):', err);
                 throw err;
