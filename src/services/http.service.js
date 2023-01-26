20dd2c533993bbe69a83d7873e9c11d8716ab177
chenleiv1 <chenleiv1@gmail.com>
2023-01-26 15:32:56 +0200

    update local

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import { router } from '@/router';
 
 const BASE_URL =
-    process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/';
+    process.env.NODE_ENV === 'production' ? '/api/' : 'https://trellor.onrender.com/api/';
 
 var axios = Axios.create({
     withCredentials: true,
